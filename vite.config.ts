d5bc66a1810046301d668f6fe0c18652ed82124a
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-09 11:53:27 +0200

    Build from src HTML entry

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'src/index.html'),
+    },
+  },
 })
